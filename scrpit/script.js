489cb3a6f3fcee36e52209ed4b7d3328f28bf086
ppji28 <pjn4tz@gmail.com>
2023-09-11 22:30:50 +0700

    Edit styles

---
+++ scrpit/script.js
@@ -0,0 +1,23 @@
+function addTask() {
+    const taskInput = document.getElementById("taskInput");
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") return;
+
+    const taskList = document.getElementById("taskList");
+
+    const li = document.createElement("li");
+    li.innerHTML = `
+        <span>${taskText}</span>
+        <button onclick="removeTask(this)">Delete</button>
+    `;
+
+    taskList.appendChild(li);
+    taskInput.value = "";
+}
+
+function removeTask(button) {
+    const taskList = document.getElementById("taskList");
+    const li = button.parentElement;
+    taskList.removeChild(li);
+}
